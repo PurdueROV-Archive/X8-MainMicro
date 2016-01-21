6ed9be88eb69f6c6394ad0722470e6d7898d9aa8
Ben Maxfield <bmaxfie@purdue.edu>
2016-01-20 22:23:30 -0500

    Worked on thrust_mapper.

--- inc/thrust_mapper.h
+++ inc/thrust_mapper.h
@@ -6,37 +6,77 @@
 
 typedef struct {
 
-	vect3 t1_l;
-	vect3 t2_l;
-	vect3 t3_l;
-	vect3 t4_l;
-	vect3 t5_l;
-	vect3 t6_l;
-	vect3 t7_l;
-	vect3 t8_l;
+	// t1_l = thruster1_location = (x,y,z) dist (m) from center of mass.
+	vect3 t1;
+	vect3 t2;
+	vect3 t3;
+	vect3 t4;
+	vect3 t5;
+	vect3 t6;
+	vect3 t7;
+	vect3 t8;
 
-	vect3 t1_r;
-	vect3 t2_r;
-	vect3 t3_r;
-	vect3 t4_r;
-	vect3 t5_r;
-	vect3 t6_r;
-	vect3 t7_r;
-	vect3 t8_r;
+} ThrusterLocationData;
 
-} ThrusterPositionData;
+typedef struct {
+
+	// t1_r = thruster1_rotation = (x-axis-angle, y-axis-angle, z-axis-angle) degrees, from...?
+	vect3 t1;
+	vect3 t2;
+	vect3 t3;
+	vect3 t4;
+	vect3 t5;
+	vect3 t6;
+	vect3 t7;
+	vect3 t8;
+
+} ThrusterRotationData;
+
+typedef struct {
+
+	MapperMatrixIndex currentMapperMatrix = ALL;
+
+	matrix8_6 mapperMatrices[] = {
+		matrix8_6 all,
+		matrix8_6 minus_t1,
+		matrix8_6 minus_t2,
+		matrix8_6 minus_t3,
+		matrix8_6 minus_t4,
+		matrix8_6 minus_t5,
+		matrix8_6 minus_t6,
+		matrix8_6 minus_t7,
+		matrix8_6 minus_t8
+	};
+
+} ThrustMapperMatrices;
+
+
+enum MapperMatrixIndex {
+	ALL = 0,
+	T1,
+	T2,
+	T3,
+	T4,
+	T5,
+	T6,
+	T7,
+	T8
+}
 
 
 class ThrustMapper
-{
+{	
 	public:
 		ThrustMapper(void);
-
+		matrix8_3 calculateThrustMap(vect6 target_vector);
+		void changeMapperMatrix(int* enabled_thrusters);
+		vect6 desired_force_vector;
+		matrix8_3 thrust_map; //??? Not sure what size the resultant thrustmap should look like.
 
 	private:
-		
-
-
+		ThrusterLocationData locations;
+		ThrusterRotationData rotations;
+		ThrustMapperMatrices mapper_matrices;
 
 };
 
--- src/thrust_mapper.cpp
+++ src/thrust_mapper.cpp
@@ -12,8 +12,35 @@
    will have no return type. */
 
 
-ThrustMapper::ThrustMapper()
+ThrustMapper::ThrustMapper(void)
 {
+	/*
+	 *	Initialize thruster location data here
+	 */
 
+	/*
+	 *	Initialize thruster rotation data here
+	 */
+
+	/*
+	 *	Initialize mapper matrices here
+	 */
+
+
+}
+
+matrix8_3 ThrustMapper::calculateThrustMap(vect6 target_vector)
+{
+	desired_force_vector = target_vector;
+
+	// Calculations:
+
+	return thrust_map;
+}
+
+// Only accepts an array of 8 ints. These ints correspond to whether each of the 8 thrusters are enabled.
+void ThrustMapper::changeMapperMatrix(MapperMatrixIndex matindex)
+{
+	mapper_matrices.currentMapperMatrix = matindex;
 }
 
