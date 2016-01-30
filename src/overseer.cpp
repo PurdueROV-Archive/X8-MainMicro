4cfcf36c0adb71f3bf760f196ec414e4a7c6b98d
Ben Maxfield <bmaxfie@purdue.edu>
2016-01-30 13:16:06 -0500

    Finished commenting Overseer.

--- src/overseer.cpp
+++ src/overseer.cpp
@@ -4,6 +4,7 @@
 
 // CLASS FUNCTIONS:
 
+// CONSTRUCTOR:
 Overseer::Overseer(void)
 {
 	int i;
@@ -18,17 +19,20 @@ Overseer::Overseer(void)
 }
 
 
-
+// Run when new input comes from Control. 
+// Calculates the thrust for each motor, and pushes to the motors.
 void Overseer::update(vect6 force, vect3 pivotPos, char on_off)
 {
 	thrustMapper.adjustPivotPosition(pivotPos);
 	thrustMapper.changeMapperMatrix(on_off);
 	thrustMapper.calculateThrustMap(force);
+	// send the thrustMapper.thrust_map to the motors (thrusters) here:
 }
 
 
-
+// Will update information about the motors.
 void Overseer::updateFromThrusters(void)
 {
-
+	// Update Current
+	// Update Voltage
 }
