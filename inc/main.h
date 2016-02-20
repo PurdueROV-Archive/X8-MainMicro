ff9c05fa3f2d13446d07623201e5e0822e7ce1e2
Ryan McBee <rmcbee@purdue.edu>
2016-02-20 15:28:18 -0500

    fixing main.h

--- inc/main.h
+++ inc/main.h
@@ -11,7 +11,23 @@
 #include "init.h"
 
 //extern volatile uint_fast8_t RampTicker;
+#define POW_CAN_ID	0x11
+#define APP_CAN_ID 	0x12
+#define CAN_ID 0x13
 
+#define GREEN 0
+#define ORANGE 1
+#define RED 2
+#define BLUE 3
+
+#define Main_Camera_Servo_PWM_Pin GPIO_PIN_0
+#define Main_Camera_Servo_PWM_GPIO_Port GPIOA
+#define Alt_Camera_Servo_PWM_Pin GPIO_PIN_3
+#define Alt_Camera_Servo_PWM_GPIO_Port GPIOA
+#define Sonar_DAC_Pin GPIO_PIN_4
+#define Sonar_DAC_GPIO_Port GPIOA
+#define Sonar_Pwm_Pin GPIO_PIN_5
+#define Sonar_Pwm_GPIO_Port GPIOA
 
 
 
